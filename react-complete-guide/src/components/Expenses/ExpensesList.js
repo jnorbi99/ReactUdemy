90986d361af642c360782a7bf6c56d9418f07478
Juhász Norbert <h870945@stud.u-szeged.hu>
2022-02-05 13:37:35 +0100

    Class-based Components vs Functional Components

--- react-complete-guide/src/components/Expenses/ExpensesList.js
+++ react-complete-guide/src/components/Expenses/ExpensesList.js
@@ -4,10 +4,6 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = (props) => {
-  if (props.items.length === 0) {
-    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
-  }
-
   return (
     <ul className="expenses-list">
       {props.items.map((item) => (
