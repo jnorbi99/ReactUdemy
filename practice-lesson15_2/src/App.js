f3ad3b7cc0a84403c184a82201e48a471e14323e
Juhász Norbert <h870945@stud.u-szeged.hu>
2022-02-09 22:45:04 +0100

    Create and use costum Hooks

---
+++ practice-lesson15_2/src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useFetch from "./hooks/use-fetch";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useFetch();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-2c7db-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ practice-lesson15_2/src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useFetch from "../../hooks/use-fetch";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useFetch();
+
+  const saveTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: "https://react-http-2c7db-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      saveTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
